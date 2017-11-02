745a781f37af9325aff38218d996ba8413941a35
Yanke Guo <ryan@islandzero.net>
2017-11-02 17:48:23 +0800

    renamed to ireul.com/bolt

--- cmd/bolt/main_test.go
+++ cmd/bolt/main_test.go
@@ -12,8 +12,8 @@ import (
 	"strconv"
 	"testing"
 
-	"github.com/boltdb/bolt"
-	"github.com/boltdb/bolt/cmd/bolt"
+	"ireul.com/bolt"
+	"ireul.com/bolt/cmd/bolt"
 )
 
 // Ensure the "info" command can print information about a database.
--- simulation_test.go
+++ simulation_test.go
@@ -7,7 +7,7 @@ import (
 	"sync"
 	"testing"
 
-	"github.com/boltdb/bolt"
+	"ireul.com/bolt"
 )
 
 func TestSimulate_1op_1p(t *testing.T)     { testSimulate(t, 1, 1) }
